c9a5a0736dcd628a75ad49fa8d72ce9114286fbc
Anna Karutina <anna.karutina@khk.ee>
2022-01-05 13:59:30 +0200

    Loodud Person klass ja tema objektid

---
+++ praks2/person_test.py
@@ -0,0 +1,11 @@
+from praks2.person import Person
+
+anna = Person()
+anna.setPerson('Anna', 41)
+anna.info()
+anna.birthday()
+anna.info()
+
+margus = Person()
+margus.setPerson('Margus', 45)
+margus.info()
